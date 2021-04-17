b1cccd8a89ddf860f5aabefcb7eddea6d468bd57
Kirill Lyahnovich <kirill.lyahnovich@gmail.com>
2021-04-17 19:50:55 +0300

    Initial Commit

---
+++ Swift UI Lab 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift UI Lab 7
+//
+//  Created by Admin on 17.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

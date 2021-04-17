f44d075d070003fbfb1518cb6e0018a3a0461942
Kirill Lyahnovich <kirill.lyahnovich@gmail.com>
2021-04-17 21:34:01 +0300

    Task 1

--- Swift UI Lab 7/ViewController.swift
+++ Swift UI Lab 7/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BGSwitch: UISwitch!
+    @IBOutlet weak var SwitchLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        SwitchLabel.textColor = UIColor.white
+        SwitchLabel.text = "Background image: BG1.jpg"
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "BG2")!)
     }
 
-
+    
+    @IBAction func SwitchToggled(_ sender: Any) {
+        if BGSwitch.isOn
+         {
+         SwitchLabel.text = "Background image: BG1.jpg"
+         view.backgroundColor = UIColor(patternImage: UIImage(named: "BG1")!)
+         }
+        else
+         {
+         SwitchLabel.text = "Background image: BG2.jpg"
+         view.backgroundColor = UIColor(patternImage: UIImage(named: "BG2")!)
+         }
+    }
+    
+  
+    
+    
 }
 
